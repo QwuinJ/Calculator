4974d7608bc63f20d6f414eb67d577e26b03cd8d
QwuinJ <jackbelfield8@gmail.com>
2021-12-03 16:21:53 +0000

    calculator completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import calcButtons from "./buttons.json";
+import { evaluate } from "mathjs";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [sum, setSum] = useState([""]);
+	const [error, setError] = useState({ isError: false, message: "" });
+
+	const handleButton = (val) => {
+		try {
+			setError({ isError: false, message: "" });
+			if (val === "=") {
+				let storedSum = [...sum];
+				let joinedSum = storedSum.join("");
+				let newSum = evaluate(joinedSum);
+				setSum([newSum]);
+			} else if (val === "clear") {
+				setSum([""]);
+			} else {
+				let storedSum = [...sum, val];
+				setSum(storedSum);
+			}
+		} catch (err) {
+			setError({ isError: true, message: err });
+			setSum([""]);
+		}
+	};
+	return (
+		<div className="App">
+			<h1>CN Calc</h1>
+			{error.isError && <div>You can't do that. try again.</div>}
+			<div className="wrapper">
+				<div className="screen">{sum}</div>
+				<div className="buttonBox">
+					<div className="buttons">
+						{calcButtons.map((item, index) => (
+							<button
+								className={item.style}
+								key={index}
+								onClick={() => {
+									handleButton(item.val);
+								}}
+							>
+								{item.val}
+							</button>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default App;
